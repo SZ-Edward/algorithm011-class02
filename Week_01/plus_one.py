3106fb17339899f7326809e2b4e98ac3a832f69d
aurora <wuxy91@gmail.com>
2020-06-23 16:01:49 +0800

    加一 代码优化

--- Week_01/plus_one.py
+++ Week_01/plus_one.py
@@ -4,20 +4,9 @@
   https://leetcode-cn.com/problems/plus-one/
 """
 def plusOne(digits):
-    if len(digits) == 0:
-        return [1]
-    for each in digits:
-        if each > 9:
-            return    
-    if len(digits) == 1:
-        if digits[0] == 9:
-            return [1, 0]
-        return [digits[0] + 1]
-
-    length = len(digits)
-    result = 0
-    for each in digits:
-        length -= 1
-        result += each * (10 ** length)
-    result += 1    
-    return [int(c) for c in str(result)]
+    for i in range(len(digits)):
+        if digits[~i] < 9:
+            digits[~i] += 1
+            return digits
+        digits[~i] = 0
+    return [1] + [0] * len(digits)
