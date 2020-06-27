60d2155c60050b8369235056f83e0afdb2c63dce
aurora <wuxy91@gmail.com>
2020-06-27 16:55:13 +0800

    完成 leetcode 有效的括号

---
+++ Week_01/valid_parentheses.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python
+# encoding: utf-8
+"""
+  https://leetcode-cn.com/problems/valid-parentheses
+"""
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {"(": ")", "[": "]", "{": "}", "?": "?"}
+        stack = ["?"]
+        for c in s:
+            if c in dic:
+                stack.append(c)
+            elif dic[stack.pop()] != c:
+                return False
+        return len(stack) == 1
