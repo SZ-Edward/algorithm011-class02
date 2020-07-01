6d02c65be8e44b2e7f80a90103e26da9e1112cc4
aurora <wuxy91@gmail.com>
2020-07-01 19:12:49 +0800

    完成 leetcode 用队列实现栈

---
+++ Week_02/implement_stack_using_queues.py
@@ -0,0 +1,38 @@
+# !/usr/bin/env python
+# encoding: utf-8
+"""
+  https://leetcode-cn.com/problems/implement-stack-using-queues/
+"""
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = collections.deque()
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.queue.append(x)
+        for _ in range(len(self.queue) - 1):
+          self.queue.append(self.queue.popleft())
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return not len(self.queue)
