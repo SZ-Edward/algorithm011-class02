9f64f61ec0ab538ec17b2505431adec92f2b11e3
aurora <wuxy91@gmail.com>
2020-07-07 16:03:42 +0800

    完成 leetcode 二叉树的最大深度

---
+++ Week_03/maximum_depth_of_binary_tree.java
@@ -0,0 +1,11 @@
+# !/usr/bin/env python
+# encoding: utf-8
+"""
+  https://leetcode-cn.com/problems/maximum-depth-of-binary-tree
+"""
+class Solution {
+    public int maxDepth(TreeNode root) {
+      if (root == null) return 0;
+      return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
